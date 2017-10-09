f10e6ac138ebd090e88539a39d5b3e4f2fe95a0d
Julian Schönbächler <info@julian-s.ch>
2017-10-09 23:21:17 +0200

    WIP: Player lobby example (unfinished).

---
+++ Assets/Volplane/Examples/Lobby/Scripts/Lobby.cs
@@ -0,0 +1,127 @@
+﻿namespace Volplane.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+    using Volplane;
+
+    public class Lobby : VolplaneBehaviour
+    {
+        [Range(2, 6)] public int minPlayerCount = 2;
+        [Range(2, 6)] public int maxPlayerCount = 6;
+        public Player playerPrefab;
+        public Transform[] playerPositions;
+
+        private List<int> playerSlots;
+
+        private void Start()
+        {
+            playerSlots = new List<int>(6) { -1, -1, -1, -1, -1, -1 };
+
+            SetStandardView("waiting");
+
+            if(playerPositions.Length > 6)
+                Debug.LogError("[Lobby] Cannot assign more than 6 player positions.");
+        }
+
+        private void OnConnect(int playerId)
+        {
+            if(PlayerCount <= 6)
+            {
+                Player playerObj = Instantiate<Player>(playerPrefab, GetFreeSlot(playerId), Quaternion.identity);
+                playerObj.playerId = playerId;
+
+                if(playerId == GetMasterId())
+                    playerObj.SetMaster(true);
+
+                SetActive(playerId, true);
+                ChangeView(playerId, "lobby");
+            }
+            else
+            {
+                // Spectating
+                SetActive(playerId, false);
+                ChangeView(playerId, "full");
+            }
+        }
+
+        private void OnDisconnect(int playerId)
+        {
+            Player playerObj = GameObject.FindObjectsOfType<Player>().FirstOrDefault(x => x.playerId == playerId);
+
+            if(playerObj == null)
+                return;
+            
+            Destroy(playerObj.gameObject);
+
+            SetFreeSlot(playerId);
+
+            if(playerId == GetMasterId())
+            {
+                // Master left
+            }
+            else if(PlayerCount >= 6)
+            {
+                int waitingPlayerId = GetAllInactivePlayers().First(x => x.IsConnected).PlayerId;
+
+                playerObj = Instantiate<Player>(playerPrefab, GetFreeSlot(waitingPlayerId), Quaternion.identity);
+                playerObj.playerId = waitingPlayerId;
+
+                // Move player to lobby
+                SetActive(playerId, true);
+                ChangeView(waitingPlayerId, "lobby");
+            }
+        }
+
+        private void OnHero(int playerId)
+        {
+            // It could be that the game master changed...
+            foreach(Player player in GameObject.FindObjectsOfType<Player>())
+            {
+                if(player.playerId == GetMasterId())
+                    player.SetMaster(true);
+                else
+                    player.SetMaster(false);
+            }
+        }
+
+        private Vector3 GetFreeSlot(int playerId)
+        {
+            for(int i = 0; i < playerPositions.Length; i++)
+            {
+                if(playerSlots[i] == -1)
+                {
+                    playerSlots[i] = playerId;
+                    return playerPositions[i].position;
+                }
+            }
+
+            return Vector3.zero;
+        }
+
+        private void SetFreeSlot(int playerId)
+        {
+            for(int i = 0; i < playerPositions.Length; i++)
+            {
+                if(playerSlots[i] == playerId)
+                {
+                    playerSlots[i] = -1;
+                    return;
+                }
+            }
+        }
+
+
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                print(VolplaneController.AirConsole.GetMasterControllerDeviceId());
+                print(GetMasterId());
+            }
+        }
+
+	}
+}
---
+++ Assets/Volplane/Examples/Lobby/Scripts/Player.cs
@@ -0,0 +1,114 @@
+﻿namespace Volplane.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Volplane;
+
+    public class Player : VolplaneBehaviour
+    {
+        public GameObject crown;
+        public GameObject body;
+        public int playerId;
+
+        private PlayerColors currentColor;
+        private Renderer objectRenderer;
+        private ElementProperties elementProp;
+
+        private enum PlayerColors
+        {
+            Red = 0,
+            Green,
+            Blue,
+            Yellow,
+            White,
+            Black
+        }
+
+        public void SetMaster(bool value)
+        {
+            crown.SetActive(value);
+        }
+
+        private void Start()
+        {
+            objectRenderer = body.GetComponent<Renderer>();
+            elementProp = new ElementProperties();
+
+            ApplyColor(PlayerColors.Red);
+        }
+
+        private void Update()
+        {
+            if(VInput.GetButtonDown(playerId, "colorChangeLeft"))
+            {
+                if(currentColor == PlayerColors.Red)
+                    currentColor = PlayerColors.Black;
+                else
+                    currentColor--;
+
+                ApplyColor(currentColor);
+            }
+
+            if(VInput.GetButtonDown(playerId, "colorChangeRight"))
+            {
+                if(currentColor == PlayerColors.Black)
+                    currentColor = PlayerColors.Red;
+                else
+                    currentColor++;
+
+                ApplyColor(currentColor);
+            }
+        }
+
+        private void ApplyColor(PlayerColors color)
+        {
+            Color newColor;
+
+            switch(color)
+            {
+                case PlayerColors.Red:
+                    newColor = new Color(231f / 256f,
+                                         76f / 256f,
+                                         60f / 256f);
+                    break;
+
+                case PlayerColors.Green:
+                    newColor = new Color(46f / 256f,
+                                         204f / 256f,
+                                         113f / 256f);
+                    break;
+
+                case PlayerColors.Blue:
+                    newColor = new Color(52f / 256f,
+                                         152f / 256f,
+                                         219f / 256f);
+                    break;
+
+                case PlayerColors.Yellow:
+                    newColor = new Color(255f / 256f,
+                                         235f / 256f,
+                                         95f / 256f);
+                    break;
+
+                case PlayerColors.White:
+                    newColor = new Color(189f / 256f,
+                                         195f / 256f,
+                                         199f / 256f);
+                    break;
+
+                default:
+                    newColor = new Color(21f / 256f,
+                                         31f / 256f,
+                                         28f / 256f);
+                    break;
+                    
+            }
+
+            objectRenderer.material.color = newColor;
+            elementProp.FontColor = newColor;
+            elementProp.Text = color.ToString("G");
+            GetPlayer(playerId).ChangeElementProperties("colorText", elementProp);
+        }
+    }
+}
